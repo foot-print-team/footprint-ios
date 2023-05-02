9c215bd78fecee117fcff7fcf86bdb235bb7edd6
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-05-02 17:12:40 +0900

    initial commit

---
+++ foot-print-project/foot-print-project/foot_print_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  foot_print_projectApp.swift
+//  foot-print-project
+//
+//  Created by Phil on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct foot_print_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
